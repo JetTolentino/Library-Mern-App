e544e7b73791d3f488cbba86b15c9ac672afbb93
Feast PICC AM_Clear <macbookhd@Clear-Feast-PICC-AMs-MacBook-Pro.local>
2022-02-23 20:46:19 +0800

    heroku commit

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI)
   console.log('Connected to database')
  )
 .then(
-  app.listen(process.env.PORT || 5000, ()=>{
+  app.listen(process.env.PORT, ()=>{
     console.log(`server listening at port 5000`)
   })
 )
